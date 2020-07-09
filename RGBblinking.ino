d6975535c96297ba13f0eae419191ed8223deda1
clairefeddema <67806894+clairefeddema@users.noreply.github.com>
2020-07-09 16:44:51 -0700

    Update RGBblinking.ino

--- RGBblinking.ino
+++ RGBblinking.ino
@@ -1,13 +1,13 @@
 const int redLED = 12;
-const int blueLED = 8;
-const int greenLED = 7;
+const int greenLED = 8;
+const int blueLED = 7;
 int waitTime = 500;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(redLED, OUTPUT);
-pinMode(blueLED, OUTPUT);
 pinMode(greenLED, OUTPUT);
+pinMode(blueLED, OUTPUT);
 }
 
 void loop() {
@@ -16,12 +16,12 @@ digitalWrite(redLED, HIGH);
 delay(waitTime);
 digitalWrite(redLED, LOW);
 delay(waitTime);
-digitalWrite(blueLED, HIGH);
-delay(waitTime);
-digitalWrite(blueLED, LOW);
-delay(waitTime);
 digitalWrite(greenLED, HIGH);
 delay(waitTime);
 digitalWrite(greenLED, LOW);
+delay(waitTime);
+digitalWrite(blueLED, HIGH);
+delay(waitTime);
+digitalWrite(blueLED, LOW);
 delay(waitTime);
 }
