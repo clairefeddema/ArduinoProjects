1bfa3e8f080aa67d6619c997f2b31a73bd7bb299
clairefeddema <67806894+clairefeddema@users.noreply.github.com>
2020-07-09 17:21:18 -0700

    Add files via upload

---
+++ sevenLEDtest.ino
@@ -0,0 +1,31 @@
+const int led1 = 12;
+const int led2 = 10;
+const int led3 = 8;
+const int led4 = 7;
+const int led5 = 5;
+const int led6 = 3;
+const int led7 = 2; 
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+pinMode(led5, OUTPUT);
+pinMode(led6, OUTPUT);
+pinMode(led7, OUTPUT);
+
+digitalWrite(led1, HIGH);
+digitalWrite(led2, HIGH);
+digitalWrite(led3, HIGH);
+digitalWrite(led4, HIGH);
+digitalWrite(led5, HIGH);
+digitalWrite(led6, HIGH);
+digitalWrite(led7, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
